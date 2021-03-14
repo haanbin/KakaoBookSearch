ca19cac0c30eb4242b9aa6a5c58a5d4e40abf234
Hanbin <haanbin@naver.com>
2021-03-14 23:39:02 +0900

    큰 이미지 처리 추가

--- app/src/main/java/com/test/kakaobooksearch/ext/ImageViewExt.kt
+++ app/src/main/java/com/test/kakaobooksearch/ext/ImageViewExt.kt
@@ -1,21 +1,25 @@
 package com.test.kakaobooksearch.ext
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.test.kakaobooksearch.R
 
-@BindingAdapter("bind:imageUrl")
-fun ImageView.setImageUrl(url: String?) {
+@BindingAdapter(
+    "bind:imageUrl",
+    "bind:error"
+)
+fun ImageView.setImageUrl(url: String?, error: Drawable) {
     url?.let {
         if (it.isNotEmpty()) {
             Glide.with(context)
                 .load(url)
-                .error(R.drawable.ic_no_image)
+                .error(error)
                 .into(this@setImageUrl)
         } else {
-            setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_no_image))
+            setImageDrawable(error)
         }
     }
 }
