5382aef85ec55426981716a6c631d4247f9f0934
Hanbin <haanbin@naver.com>
2021-03-14 18:04:41 +0900

    불필요 메서드 제거

--- app/src/main/java/com/test/kakaobooksearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/test/kakaobooksearch/ui/main/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.test.kakaobooksearch.ui.main
 
-import android.content.res.Resources
-import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.navigation.fragment.NavHostFragment
 import com.test.kakaobooksearch.BR
 import com.test.kakaobooksearch.R
 import com.test.kakaobooksearch.base.BaseActivity
 import com.test.kakaobooksearch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
@@ -17,24 +13,4 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
     override val viewModel: MainViewModel by viewModels()
     override val viewModelVariable: Int
         get() = BR.vm
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        start()
-    }
-
-    private fun start() {
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment? ?: return
-        val navController = host.navController
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            val dest: String = try {
-                resources.getResourceName(destination.id)
-            } catch (e: Resources.NotFoundException) {
-                destination.id.toString()
-            }
-            Timber.d("Navigated to $dest")
-        }
-    }
-
 }
