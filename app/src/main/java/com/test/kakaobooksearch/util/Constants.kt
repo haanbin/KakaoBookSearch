37afa5d908156a5f72f7787c3ef0e8ea94b2bb6f
Hanbin <haanbin@naver.com>
2021-06-18 22:43:26 +0900

    불필요 상수 제거

--- app/src/main/java/com/test/kakaobooksearch/util/Constants.kt
+++ app/src/main/java/com/test/kakaobooksearch/util/Constants.kt
@@ -2,17 +2,10 @@ package com.test.kakaobooksearch.util
 
 object Constants {
 
-    const val KAKAO_API_KEY = "KakaoAK"
-    const val AUTHORIZATION = "Authorization"
-    const val CONTENT_TYPE = "Content-Type"
-    const val APPLICATION_JSON = "application/json"
     const val QUERY = "query"
     const val PAGE = "page"
     const val SIZE = "size"
     const val DEFAULT_PAGE_VALUE = 1
     const val DEFAULT_SIZE_VALUE = 50
-    const val NETWORK_NEED_SECOND = 60 * 5
     const val ARG_DOCUMENT = "document"
-    const val RESULT_ERROR_NULL = "RESULT_IS_NULL"
-    const val RESULT_ERROR_BODY_NULL = "RESULT_IS_NULL"
 }
